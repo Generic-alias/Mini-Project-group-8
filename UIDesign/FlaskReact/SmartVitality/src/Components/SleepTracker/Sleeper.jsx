dc3a61eb1cd60e4b03245be1fe28420915bdb4d2
kali <kali@kali>
2025-03-22 22:48:43 +0530

    Conflict resolved

--- UIDesign/FlaskReact/SmartVitality/src/Components/SleepTracker/Sleeper.jsx
+++ UIDesign/FlaskReact/SmartVitality/src/Components/SleepTracker/Sleeper.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios'
-=======
 import './SleepTrack.css';
 
->>>>>>> f5c5b3530557eaf11675af8964dc820a76cdaf23
 function SleepTrack(){
     const [sleepEfficiency, setSleepEfficiency] = useState(null);
     const [data, setData] = useState({"age": "", 
