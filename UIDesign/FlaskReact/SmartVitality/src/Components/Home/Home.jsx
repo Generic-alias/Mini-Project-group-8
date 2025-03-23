0f3009a207492bb67f0c4a78679d9762c833d24b
AdityaRajSinghTomar <adityatomar2332004@gmail.com>
2025-03-23 18:29:47 +0530

    sorry bhai

--- UIDesign/FlaskReact/SmartVitality/src/Components/Home/Home.jsx
+++ UIDesign/FlaskReact/SmartVitality/src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
+
 import './Home.css';
-=======
+
 import './Home.css'
->>>>>>> d62e97b6fb397d91c0e10446c21137f22e6fbaa2
+
 function Home(){
     return(
         <div>
@@ -15,21 +15,21 @@ function Home(){
         <Link to="/SleepTrack">
              Sleep Tracking
           </Link>
-<<<<<<< HEAD
+
 
           <Link to = "/">
           <p>Diet Tracking</p>
          
           </Link>
-        </>
-=======
+
+
         <Link to = "/DietTrack">
         Diet Tracking
         </Link>
         </div>
         <h1>Welcome To Smart Vitality Tracker with AI</h1>
-        </ div>
->>>>>>> d62e97b6fb397d91c0e10446c21137f22e6fbaa2
+        </div>
+
     );
 }
 
