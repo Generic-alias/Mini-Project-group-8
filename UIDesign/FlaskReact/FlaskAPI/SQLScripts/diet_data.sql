6beb417d7e19bd799e4decf646553528e41e0531
AdityaRajSinghTomar <adityatomar2332004@gmail.com>
2025-03-31 17:22:59 +0530

    SQL Scripts for required tables(use database mini)

---
+++ UIDesign/FlaskReact/FlaskAPI/SQLScripts/diet_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `diet_data` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(1024) DEFAULT NULL,
+  `serving_in_g` bigint DEFAULT NULL,
+  `calories` bigint DEFAULT NULL,
+  `protein` bigint DEFAULT NULL,
+  `cholesterol` bigint DEFAULT NULL,
+  `carbohydrate` bigint DEFAULT NULL,
+  `total_fat` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
